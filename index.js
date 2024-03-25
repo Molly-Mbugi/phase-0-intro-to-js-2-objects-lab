256107367f9d769ae12a9119e3e8a58b46f94f26
molly <mollynzembi@gmail.com>
2024-03-25 22:49:35 +0300

    creates index.js

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+  }
+  
+  // Test the function
+  let employee = {
+    name: "John Doe",
+    streetAddress: "123 Main St"
+  };
+  
+  let updatedEmployee = updateEmployeeWithKeyAndValue(employee, 'streetAddress', '456 Elm St');
+  
+  console.log(updatedEmployee);
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  // Function to delete property with specified key from a clone of the employee object (non-destructive)
+  function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = { ...employee };
+    delete newEmployee[key];
+    return newEmployee;
+  }
+  
+  // Function to delete property with specified key from the employee object (destructive)
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
+  
+  
